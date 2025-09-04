1fbdf8af84ed0a9be3b92cb350743c05549b401c
Xavier Lermusieaux <xavier.lermusieaux@gmail.com>
2025-09-04 21:47:18 +0100

    chore: formatting

--- go-server/dispense.go
+++ go-server/dispense.go
@@ -1,8 +1,10 @@
 package main
 
-import ("fmt"
-		"github.com/stianeikeland/go-rpio"
-	)
+import (
+	"fmt"
+
+	"github.com/stianeikeland/go-rpio"
+)
 
 func main() {
 	fmt.Println("GPIO Setup")
@@ -10,6 +12,5 @@ func main() {
 	if err != nil {
 		panic(fmt.Sprint("unable to open gpio", err.Error()))
 	}
-
 	defer rpio.Close()
 }
